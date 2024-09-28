c335c0955be955ad3908f6094a411010046cbf77
JoisFe <rhsnqk@gmail.com>
2024-09-28 23:43:58 +0900

    Title: hashmap 기본, Time: 726ms, Memory: 21MB, Status: Passed - CodeTree

---
+++ 240928/hashmap 기본/hashmap-basic.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n; ++i) {
+            st = new StringTokenizer(br.readLine());
+
+            String cmd = st.nextToken();
+             
+            switch (cmd) {
+                case "add": 
+                map.put(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+                break;
+                
+                case "find":
+                Integer findKey = Integer.parseInt(st.nextToken());
+                if (map.containsKey(findKey)) {
+                    System.out.println(map.get(findKey));
+                } else {
+                    System.out.println("None");
+                }
+                break;
+
+                case "remove":
+                map.remove(Integer.parseInt(st.nextToken()));
+                break;
+            }
+        }
+    }
+}

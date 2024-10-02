2436921cdd99ccd7c75663b36f8eff0b0a0d4175
JoisFe <rhsnqk@gmail.com>
2024-10-02 23:40:40 +0900

    Title: 연속 부분 합의 최댓값 구하기 2, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241002/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.java
+++ 241002/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        int[] nums = new int[n ];
+        int[] nums = new int[n];
 
         for (int i = 0; i < n; ++i) {
             nums[i] = sc.nextInt();

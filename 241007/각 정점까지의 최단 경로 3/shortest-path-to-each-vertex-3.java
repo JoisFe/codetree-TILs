805eb18602289c20d4eaaeaf9e9b6bd646249aba
JoisFe <rhsnqk@gmail.com>
2024-10-07 21:02:11 +0900

    Title: 각 정점까지의 최단 경로 3, Time: 227ms, Memory: 14MB, Status: Passed - CodeTree

--- 241007/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.java
+++ 241007/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.java
@@ -8,7 +8,7 @@ public class Main {
         int n = sc.nextInt();
         int m = sc.nextInt();
 
-        List<List<Edge> graph = new ArrayList<>();
+        List<List<Edge>> graph = new ArrayList<>();
         int[] dists = new int[n + 1];
 
         Arrays.fill(dists, INF);
@@ -23,19 +23,54 @@ public class Main {
 
         dijkstra(graph, dists, 1);
 
-        for (int i = 1; i <= n; ++i) {
+        for (int i = 2; i <= n; ++i) {
             if (dists[i] == INF) {
                 dists[i] = -1;
             }
 
-            System.out.print(dists[i] + " ");
+            System.out.println(dists[i]);
+        }
+    }
+
+    private static void dijkstra(List<List<Edge>> graph, int[] dists, int startNode) {
+
+        Queue<Edge> pq = new PriorityQueue<>();
+        pq.offer(new Edge(startNode, 0));
+        dists[startNode] = 0;
+
+        while (!pq.isEmpty()) {
+            Edge edge = pq.poll();
+            int currentNode = edge.getNode();
+            int currentDist = edge.getDist();
+
+            if (dists[currentNode] < currentDist) {
+                continue;
+            }
+
+            for (int i = 0; i < graph.get(currentNode).size(); ++i) {
+                Edge nextEdge = graph.get(currentNode).get(i);
+
+                int nextNode = nextEdge.getNode();
+                int cost = currentDist + nextEdge.getDist();
+
+                if (cost < dists[nextNode]) {
+                    dists[nextNode] = cost;
+
+                    pq.add(new Edge(nextNode, cost));
+                }
+            }
         }
     }
 }
 
 class Edge implements Comparable<Edge> {
-    private int node;
-    private int dist;
+    private final int node;
+    private final int dist;
+
+    public Edge(int node, int dist) {
+        this.node = node;
+        this.dist = dist;
+    }
 
     public int getNode() {
         return this.node;
@@ -46,7 +81,7 @@ class Edge implements Comparable<Edge> {
     }
 
     @Override
-    public int comareTo(Node other) {
+    public int compareTo(Edge other) {
         return Integer.compare(this.dist, other.dist);
     }
 }

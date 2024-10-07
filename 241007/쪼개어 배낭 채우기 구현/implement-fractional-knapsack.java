0ebfa5cf5004e5027acae2fbd201f7866e20bc97
JoisFe <rhsnqk@gmail.com>
2024-10-07 23:33:02 +0900

    Title: 쪼개어 배낭 채우기 구현, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241007/쪼개어 배낭 채우기 구현/implement-fractional-knapsack.java
@@ -0,0 +1,72 @@
+import java.util.*;
+import java.util.stream.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        List<Jewelry> jewelries = new ArrayList<>();
+        
+        for (int i = 0; i < n; ++i) {
+            jewelries.add(new Jewelry(sc.nextInt(), sc.nextInt()));
+        }
+
+        jewelries = jewelries.stream().sorted().collect(Collectors.toList());
+
+        double maxPrice = 0;
+
+        int index = 0;
+
+        while (m > 0) {
+            int weight = jewelries.get(index).getWeight();
+            int price = jewelries.get(index).getPrice();
+            double unitPrice = jewelries.get(index).getUnitPrice();
+
+            if (m >= weight) {
+                m -= weight;
+                maxPrice += price;
+
+            } else {
+                maxPrice += unitPrice * m;
+                m = 0;
+            }
+
+            ++index;
+        }
+
+        System.out.printf("%.3f", maxPrice);
+    }
+}
+
+class Jewelry implements Comparable<Jewelry> {
+    private final int weight;
+    private final int price;
+    private final double unitPrice;
+
+    public Jewelry(int weight, int price) {
+        this.weight = weight;
+        this.price = price;
+
+        this.unitPrice = (double) price / weight;
+    }
+
+    public int getWeight() {
+        return this.weight;
+    }
+
+    public int getPrice() {
+        return this.price;
+    }
+
+    public double getUnitPrice() {
+        return this.unitPrice;
+    }
+
+    @Override
+    public int compareTo(Jewelry other) {
+        return Double.compare(other.unitPrice, this.unitPrice);
+    }
+}

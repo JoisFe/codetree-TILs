038d977316d78afdddd3c4b17de65cdb7dcb1019
JoisFe <rhsnqk@gmail.com>
2024-10-08 19:53:05 +0900

    Title: 각 정점까지의 최단 경로 2, Time: 139ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241008/각 정점까지의 최단 경로 2/shortest-path-to-each-vertex-2.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+public class Main {
+
+    private static final int INF = Integer.MAX_VALUE;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[][] map = new int[n + 1][n + 1];
+
+        for (int[] innerMap : map) {
+            Arrays.fill(innerMap, INF);
+        }
+
+        for (int i = 1; i <= n; ++i) {
+            map[i][i] = 0;
+        }
+
+        for (int i = 0; i < m; ++i) {
+            map[sc.nextInt()][sc.nextInt()] = sc.nextInt();
+        }
+
+        for (int mid = 1; mid <= n; ++mid) {
+            for (int i = 1; i <= n; ++i) {
+                for (int j = 1; j <= n; ++j) {
+                    int dist = map[i][mid] + map[mid][j];
+                    
+                    if (dist < 0) {
+                        dist = INF;
+                    }
+
+                    map[i][j] = Math.min(map[i][j], dist);
+                }
+            }
+        }
+
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                int dist = map[i][j];
+                
+                if (dist == INF) {
+                    dist = -1;
+                }
+
+                System.out.print(dist + " ");
+            }
+            System.out.println();
+        }
+    }
+}

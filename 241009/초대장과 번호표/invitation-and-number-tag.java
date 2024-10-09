86d110b960cc014bfb57f1e3343399e750c43d1c
JoisFe <rhsnqk@gmail.com>
2024-10-09 17:05:05 +0900

    Title: 초대장과 번호표, Time: 170ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241009/초대장과 번호표/invitation-and-number-tag.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int g = sc.nextInt();
+
+        Set<Integer> set = new HashSet<>();
+        set.add(1);
+
+        for (int i = 0; i < g; ++i) {
+            int cnt = sc.nextInt();
+
+            int idxSize = cnt;
+            int uniqueNum = -1;
+
+            for (int j = 0; j < idxSize; ++j) {
+                int num = sc.nextInt();
+
+                if (set.contains(num)) {
+                    --cnt;
+                    continue;
+                }
+
+                uniqueNum = num;
+            }
+
+            if (cnt == 1) {
+                set.add(uniqueNum);
+            }
+        }
+
+        System.out.println(set.size());
+    }
+}

dd044246d1e151b73fc487a9ac9519f29dfbc0d6
JoisFe <rhsnqk@gmail.com>
2024-10-09 18:05:27 +0900

    Title: 초대장과 번호표, Time: 2039ms, Memory: 106MB, Status: Passed - CodeTree

--- 241009/초대장과 번호표/invitation-and-number-tag.java
+++ 241009/초대장과 번호표/invitation-and-number-tag.java
@@ -1,45 +1,68 @@
 import java.util.*;
 
 public class Main {
+
+    private static int answer = 0;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
         int g = sc.nextInt();
 
-        Set<Integer> set = new HashSet<>();
-        set.add(1);
+        List<Set<Integer>> groupSetList = new ArrayList<>();
+        List<List<Integer>> groupGraph = new ArrayList<>();
 
-        while (true) {
-            int setSize = set.size();
+        boolean[] check = new boolean[n + 1];
 
-            for (int i = 0; i < g; ++i) {
-                int cnt = sc.nextInt();
-
-                int idxSize = cnt;
-                int uniqueNum = -1;
-
-                for (int j = 0; j < idxSize; ++j) {
-                    int num = sc.nextInt();
-
-                    if (set.contains(num)) {
-                        --cnt;
-                        continue;
-                    }
-
-                    uniqueNum = num;
-                }
-
-                if (cnt == 1) {
-                    set.add(uniqueNum);
-                }
-            }
-
-            if (setSize == set.size()) {
-                break;
-            }
+        for (int i = 0; i < g; ++i) {
+            groupSetList.add(new HashSet<>());
         }
 
-        System.out.println(set.size());
+        for (int i = 0; i <= n; ++i) {
+            groupGraph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < g; ++i) {
+            int cnt = sc.nextInt();
+
+            for (int j = 0; j < cnt; ++j) {
+                int peopleNum = sc.nextInt();
+
+                groupSetList.get(i).add(peopleNum); // i번째 그룹에 num 사람이 존재
+                groupGraph.get(peopleNum).add(i); // num 사람이 i번재 그룹으로 이동가능
+            }
+        } 
+
+        bfs(groupSetList, groupGraph, check);
+
+        System.out.println(answer);
+    }
+
+    private static void bfs(List<Set<Integer>> groupSetList, List<List<Integer>> groupGraph, boolean[] check) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(1);
+        check[1] = true;
+
+        while (!q.isEmpty()) {
+            int peopleNum = q.poll();
+            ++answer;
+
+            // num 이 들어가 있는 모든 그룹 조회하면서 num은 이미 초대장 받았으니 제거 (방문 후 제거 라 보면됨)
+            for (int i = 0; i < groupGraph.get(peopleNum).size(); ++i) {
+                int nextGroup =  groupGraph.get(peopleNum).get(i);
+
+                groupSetList.get(nextGroup).remove(peopleNum);
+
+                if (groupSetList.get(nextGroup).size() == 1) {
+                    int nextPeopleNum = new ArrayList<>(groupSetList.get(nextGroup)).get(0);
+
+                    if (!check[nextPeopleNum]) {
+                        check[nextPeopleNum] = true;
+                        q.offer(nextPeopleNum);
+                    }
+                }
+            }
+        }
     }
 }
